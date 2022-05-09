5d1a30ed30c2050a503ddea409fa4972989efb53
Jianbin Huang <907310116@qq.com>
2022-05-09 15:19:12 +0800

    安装格式代码化及安装elementPlus

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// 自动导入elementPlus组件库
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: config => {
+    config.plugins.push(AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }))
+    config.plugins.push(Components({
+      resolvers: [ElementPlusResolver()]
+    }))
+  }
+}
