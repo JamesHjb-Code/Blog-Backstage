28f0b7ad24144f2383436270e874a0dfc71315b5
Jianbin Huang <907310116@qq.com>
2022-05-09 16:00:30 +0800

    引入全局样式

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="box"></div>
   <router-view/>
 </template>
-
+<script setup>
+const boxWidth = '100px'
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .box{
+    width:v-bind(boxWidth);
+    height:100px;
+    background-color:red;
   }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,13 @@
 <template>
+ <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+  </el-row>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
